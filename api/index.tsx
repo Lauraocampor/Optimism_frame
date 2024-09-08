d6b57b26093918c0e65cce761bb8552b04bd0747
AndresSCV <33283680+AndresSCV@users.noreply.github.com>
2024-09-08 16:05:45 -0500

    full with h1 in other position

--- api/index.tsx
+++ api/index.tsx
@@ -281,6 +281,37 @@ intents.push(<Button.Reset>Reset</Button.Reset>);
 
 return c.res({
 image: (
+  <div style={{
+    display: 'flex',
+    background: '#f6f6f6',
+    width: '100%',
+    height: '100%',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    position: 'relative'
+  }}>
+    <img width="1200" height="630" alt="background" src={`/Frame_3_rec.png`}/>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        position: 'absolute',
+        color: '#161B33',
+        fontSize: '70px',
+        textTransform: 'uppercase',
+        letterSpacing: '-0.030em',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        boxSizing: 'border-box',
+        alignItems: 'center',
+        lineHeight: 1.4,
+        padding: '0px 50px',
+        overflow: 'hidden', 
+        textOverflow: 'ellipsis',
+        textAlign: 'center', 
+      }}>
         <div style={{
             display: 'flex',
             flexDirection: 'row',
@@ -290,45 +321,47 @@ image: (
             flexWrap: 'wrap',
             gap: '10px'
         }}>
-              <h1></h1>
-              <ul style={{
-                display: 'flex',
-                flexDirection: 'column',
-                listStyleType: 'none',
-                padding: '0',
-                margin: '0',
-                width: '40%',
-                boxSizing: 'border-box'
-              }}>
-                {delegates.map((item, index) => (
-                  <li key={index} style={{
-                    margin: '10px 0',
-                    padding: '5px',
-                    borderBottom: '1px solid #ddd',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    whiteSpace: 'nowrap',
-                  }}>{item.address}</li>
-                ))}
-              </ul>
-            
-              <ul style={{
-                display: 'flex',
-                flexDirection: 'column',
-                listStyleType: 'none',
-                padding: 0,
-                margin: '0',
-                width: '20%',
-                boxSizing: 'border-box',
-              }}>
-                {delegates.map((item, index) => (
-                  <li key={index} style={{ margin: '10px 0',
-                    padding: '5px',
-                    borderBottom: '1px solid #ddd',
-                    justifyContent: 'flex-end', }}>{item.count}</li>
-                ))}
-              </ul>
+            <h1></h1>
+            <ul style={{
+              display: 'flex',
+              flexDirection: 'column',
+              listStyleType: 'none',
+              padding: '0',
+              margin: '0',
+              width: '40%',
+              boxSizing: 'border-box'
+            }}>
+              {delegates.map((item, index) => (
+                <li key={index} style={{
+                  margin: '10px 0',
+                  padding: '5px',
+                  borderBottom: '1px solid #ddd',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                  whiteSpace: 'nowrap',
+                }}>{item.address}</li>
+              ))}
+            </ul>
+          
+            <ul style={{
+              display: 'flex',
+              flexDirection: 'column',
+              listStyleType: 'none',
+              padding: 0,
+              margin: '0',
+              width: '20%',
+              boxSizing: 'border-box',
+            }}>
+              {delegates.map((item, index) => (
+                <li key={index} style={{ margin: '10px 0',
+                  padding: '5px',
+                  borderBottom: '1px solid #ddd',
+                  justifyContent: 'flex-end', }}>{item.count}</li>
+              ))}
+            </ul>
         </div>
+    </div>
+  </div>
 ),
 intents,
 });
